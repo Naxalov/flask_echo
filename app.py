a33a4fd93b2b15724a5f45828e5661acbf62e6af
ZARIFMAC <uzbzarif@gmail.com>
2021-08-28 13:02:01 +0900

    Query string

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import request
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-   return "Hello World!"
+   r = request.args
+   data = int(r['a'])*2
+   
+   return str(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
