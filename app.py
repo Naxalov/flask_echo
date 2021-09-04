1a31571d2336faabe1359309575c2f799fda07cd
ZARIFMAC <uzbzarif@gmail.com>
2021-09-04 10:52:42 +0900

    echo

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask import request
-
+import telegram
 
 app = Flask(__name__)
-
+bot = telegram.Bot('1602686596:AAECWOgNbMCkfTUAxYEtKJFtnej6H6Dp5TA')
 @app.route("/")
 def get_sum():
    r = request.args
@@ -16,6 +16,9 @@ def get_sum():
 def get_data():
    if request.method=='POST':
       print(request.json)
+      update = request.json['message']['text']
+      chat_id = request.json['message']['from']['id']
+      bot.sendMessage(chat_id,update)
       return {'result':0}
    return {'error':0}
 
