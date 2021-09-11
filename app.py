975eb144e7a00d683dbdc1a4e3acd5299b4b26c5
Otabek <ojuraev@gmail.com>
2021-09-11 11:02:18 +0900

    Namoz 1

--- app.py
+++ app.py
@@ -7,11 +7,33 @@ from telegram.ext import CommandHandler, Dispatcher, Filters, MessageHandler
 app = Flask(__name__)
 bot = telegram.Bot('1602686596:AAECWOgNbMCkfTUAxYEtKJFtnej6H6Dp5TA')
 
-def echo(update, context):
+button1 = KeyboardButton(text = "Shahar yoki davlat nomini inglizchada yuboring")
+button2 = KeyboardButton(text="Location yuboring", request_location = True)
+
+buttons = ReplyKeyboardMarkup([[button1], [button2]], resize_keyboard=True)
+
+def start(update, context):
+    bot =context.bot
+    update.message.reply_html(
+        '<b>Assalomu alaykum, {}</b>\n \nMen namoz vaqtlari haqida ma`lumot beruvchi botman. Shahar nomini yuboring yoki Location yuboring'.format(update.message.from_user.first_name), reply_markup=buttons)
+    return 1
+
+def location_user(update, context):
     bot = context.bot
-    text = update.message.text
-    chat_id = update.message.chat.id
-    bot.sendMessage(chat_id, 'Welcome to our bot')
+    latitude = update.message.location.latitude
+    longitude = update.message.location.longitude
+    chat_id = update.message.from_user.id
+    bot.sendMessage(chat_id, get_namoz_times(longitude, latitude))
+    
+
+def echo(update,context):
+   bot = context.bot
+   city_name = update.message.text
+   text = get_by_city(city_name)
+   chat_id = update.message.from_user.id
+   bot.sendMessage(chat_id, text)
+   print(city_name)
+
 
 @app.route('/data',methods=['POST','GET'])
 def get_data():
@@ -19,10 +41,15 @@ def get_data():
       dp = Dispatcher(bot,None,workers=0)
 
       update = telegram.Update.de_json(request.json,bot)
+
+      dp.add_handler(CommandHandler('start', start))
+      dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+      dp.add_handler(MessageHandler(Filters.location, callback=location_user))
+
       dp.add_handler(MessageHandler(Filters.text,echo))
       dp.process_update(update)
-      # print(request.json)
-      return {'result':0}
+      
+
    return {'error':0}
 
 if __name__ == '__main__':
