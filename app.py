5d3fa49189d47d11767f1b29bd88acbfa658b144
ZARIFMAC <uzbzarif@gmail.com>
2021-09-11 10:23:50 +0900

    echo

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask
 from flask import request
 import telegram
+from telegram.ext import CommandHandler, Dispatcher, Filters, MessageHandler
+
 
 app = Flask(__name__)
 bot = telegram.Bot('1602686596:AAECWOgNbMCkfTUAxYEtKJFtnej6H6Dp5TA')
 
+def echo(update, context):
+    bot = context.bot
+    text = update.message.text
+    chat_id = update.message.chat.id
+    bot.sendMessage(chat_id, 'Welcome to our bot')
+
 @app.route('/data',methods=['POST','GET'])
 def get_data():
    if request.method=='POST':
+      dp = Dispatcher(bot)
+
+      update = telegram.update.de_json(request.json(),bot)
+      dp.add_handler(MessageHandler(Filters.text,echo))
+      dp.process_update(update)
       # print(request.json)
       return {'result':0}
    return {'error':0}
