0439cf4bb0e4337cd7b4f5b46e2c7c3950024d0d
ZARIFMAC <uzbzarif@gmail.com>
2024-01-17 16:12:48 +0500

    Hello Flask

---
+++ init.sh
@@ -0,0 +1,9 @@
+# Create a virtual environment and install dependencies
+python3 -m venv venv
+# Activate the virtual environment
+source venv/bin/activate
+# Install dependencies
+pip install -r requirements.txt
+# Run the Flask app
+python3 app.py
+
